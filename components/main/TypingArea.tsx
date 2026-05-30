aff50caec05401bd9a5e720c29d44cf181aeb36c
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-05-31 00:01:30 +0530

    feat(typingArea): add typing area component

---
+++ components/main/TypingArea.tsx
@@ -0,0 +1,60 @@
+import { RefObject } from "react";
+
+type props = {
+    words:string[];
+    typedCharacters:{
+        char: string;
+        expectedChar: string;
+        correct: boolean;
+        charIndex: number;
+        wordIndex: number;}[];
+    currentWordIndex: number;
+    currentCharacterIndex: number;
+    activeCharRef: RefObject<HTMLSpanElement | null>;
+    
+}
+const TypingArea = ({words,typedCharacters,currentWordIndex,currentCharacterIndex,activeCharRef}:props) => {
+  return (
+    <div className="flex flex-wrap gap-x-3 relative" style={{fontSize:"var(--text-size)"}}>
+    {words.map((word, wordIndex) => {
+          return (
+            <div key={wordIndex} className="whitespace-nowrap">
+              {word.split("").map((char, charIndex) => {
+                const typedCharacter = typedCharacters.find(
+                  (item) =>
+                    item.wordIndex === wordIndex &&
+                    item.charIndex === charIndex,
+                );
+
+                const isCurrentChar =
+                  currentWordIndex === wordIndex &&
+                  currentCharacterIndex === charIndex;
+
+                return (
+                  <span
+                    key={charIndex}
+                    ref={isCurrentChar ? activeCharRef : null}
+                    style={{
+                      color:
+                        typedCharacter === undefined
+                          ? "gray"
+                          : typedCharacter.correct
+                            ? "white"
+                            : "red",
+                    }}
+                  >
+                    {char}
+                  </span>
+                );
+              })}
+
+              {/* REAL SPACE */}
+              {wordIndex !== words.length - 1 && <span> </span>}
+            </div>
+          );
+        })}
+        </div>
+  )
+}
+
+export default TypingArea
