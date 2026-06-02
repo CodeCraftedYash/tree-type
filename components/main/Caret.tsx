c952a268ee1d2285d18ea1c945e1f90804726296
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-06-03 01:52:17 +0530

    feat(components): add caret component

---
+++ components/main/Caret.tsx
@@ -0,0 +1,22 @@
+type props = {
+    caretStyle: {
+        left:number;
+        top:number;
+        height:number;
+    }
+}
+
+const Caret = ({caretStyle}:props) => {
+  return (
+     <div
+          className="absolute w-[1.5px] scale-y-90 bg-yellow-400 animate-pulse transition-all duration-100"
+          style={{
+            left: `${caretStyle.left}px`,
+            top: `${caretStyle.top}px`,
+            height: `${caretStyle.height}px`,
+          }}
+        />
+  )
+}
+
+export default Caret
