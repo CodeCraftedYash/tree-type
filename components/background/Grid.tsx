d0856285b19ce5c728f1ba73bd39c706830be84e
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-06-03 01:51:23 +0530

    feat(components): add component to display grid in the background

---
+++ components/background/Grid.tsx
@@ -0,0 +1,10 @@
+
+const Grid = () => {
+  return (
+    <div className="absolute top-0 left-0 w-full min-h-full -z-10"
+    style={{backgroundImage:"linear-gradient(to right, rgba(255, 255, 255, 0.05) 2px, transparent 2px),linear-gradient(to bottom, rgba(255, 255, 255, 0.05) 2px, transparent 2px)",backgroundSize:"60px 60px"}}
+    ></div>
+  )
+}
+
+export default Grid
