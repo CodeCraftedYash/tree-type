26020ef9461b434f09189de9988483b1ac0c5758
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-06-03 02:02:34 +0530

    feat(components): add tree svg

---
+++ components/svg/Tree.tsx
@@ -0,0 +1,29 @@
+const Tree = () => {
+  return (
+    <div>
+      <svg
+        viewBox="0 0 485 458"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="m212.182 456.834v-151.5h60.5v151.5z"
+          fill="#330606"
+          stroke="black"
+        />
+
+        <path
+          d="m160.182 127.334l107-126.5l83 126.5h-13l88.5 107h-16.311l73.811 86.5h-482l84-86.5h-13l100.5-107z"
+          fill="#0D4900"
+        />
+
+        <path
+          d="m172.682 127.334h-12.5l107-126.5l83 126.5h-13m-164.5 0l-100.5 107h353.5l-88.5-107m-165 0h164.5m-336 193.5l84-86.5h324.188l73.811 86.5z"
+          stroke="black"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default Tree;
