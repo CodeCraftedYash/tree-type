b2c1e42ba44983add658a6ad1d0ef3d87b50c4ea
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-06-16 02:27:45 +0530

    feat(components): add character component

---
+++ components/layout/Character.tsx
@@ -0,0 +1,36 @@
+import { memo, RefObject } from "react";
+
+type Props = {
+  char: string;
+  typedCharacter?: {
+    correct: boolean;
+  };
+  isCurrentChar: boolean;
+  activeCharRef: RefObject<HTMLSpanElement | null>;
+};
+
+const Character = memo(function Character({
+  char,
+  typedCharacter,
+  isCurrentChar,
+  activeCharRef,
+}: Props) {
+    console.log(char);
+  return (
+    <span
+      ref={isCurrentChar ? activeCharRef : null}
+      style={{
+        color:
+          typedCharacter === undefined
+            ? "gray"
+            : typedCharacter.correct
+              ? "white"
+              : "red",
+      }}
+    >
+      {char}
+    </span>
+  );
+});
+
+export default Character;
