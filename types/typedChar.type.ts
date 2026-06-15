5e301e26f6fd20c0c3a5a09cce1cdecd1087fec2
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-06-15 17:10:27 +0530

    feat(types): add typing State type and add char index and word index in typedCharacter

--- types/typedChar.type.ts
+++ types/typedChar.type.ts
@@ -1,7 +1,17 @@
-export type typedCharacters = {
+export type TypedCharacter = {
   char: string;
   expectedChar: string;
   correct: boolean;
-  charIndex: number;
-  wordIndex: number;
+  charIndex?: number;
+  wordIndex?: number;
+};
+
+export type TypingState = {
+  currentWordIndex: number;
+  currentCharacterIndex: number;
+  typedCharacters: Map<string, TypedCharacter>;
+  correctCharacters: number;
+  incorrectCharacters: number;
+  isStarted: boolean;
+  isFinished: boolean;
 };
