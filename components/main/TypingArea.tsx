e362eaba55b47d41025a8dce44505b465acded07
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-06-16 02:28:48 +0530

    refactor(components): replace the span with character component and memoize it

--- components/main/TypingArea.tsx
+++ components/main/TypingArea.tsx
@@ -1,52 +1,56 @@
 import type { TypedCharacter } from "@/types/typedChar.type";
 import { RefObject } from "react";
+import Character from "../layout/Character";
 
 type props = {
-    words:string[];
-    typedCharacters:Map<string,TypedCharacter>;
-    currentWordIndex: number;
-    currentCharacterIndex: number;
-    activeCharRef: RefObject<HTMLSpanElement | null>;
-    
-}
-const TypingArea = ({words,typedCharacters,currentWordIndex,currentCharacterIndex,activeCharRef}:props) => {
+  words: string[];
+  typedCharacters: Map<string, TypedCharacter>;
+  currentWordIndex: number;
+  currentCharacterIndex: number;
+  activeCharRef: RefObject<HTMLSpanElement | null>;
+};
+const TypingArea = ({
+  words,
+  typedCharacters,
+  currentWordIndex,
+  currentCharacterIndex,
+  activeCharRef,
+}: props) => {
   return (
-    <div className="flex flex-wrap gap-x-3 relative" style={{fontSize:"var(--text-size)"}}>
-    {words.map((word, wordIndex) => {
-          return (
-            <div key={wordIndex} className="whitespace-nowrap">
-              {word.split("").map((char, charIndex) => {
-                const typedCharacter = typedCharacters.get(`${wordIndex}-${charIndex}`);
+    <div
+      className="flex flex-wrap gap-x-3 relative"
+      style={{ fontSize: "var(--text-size)" }}
+    >
+      {words.map((word, wordIndex) => {
+        return (
+          <div key={wordIndex} className="whitespace-nowrap">
+            {word.split("").map((char, charIndex) => {
+              const typedCharacter = typedCharacters.get(
+                `${wordIndex}-${charIndex}`,
+              );
 
-                const isCurrentChar =
-                  currentWordIndex === wordIndex &&
-                  currentCharacterIndex === charIndex;
+              const isCurrentChar =
+                currentWordIndex === wordIndex &&
+                currentCharacterIndex === charIndex;
 
-                return (
-                  <span
-                    key={charIndex}
-                    ref={isCurrentChar ? activeCharRef : null}
-                    style={{
-                      color:
-                        typedCharacter === undefined
-                          ? "gray"
-                          : typedCharacter.correct
-                            ? "white"
-                            : "red",
-                    }}
-                  >
-                    {char}
-                  </span>
-                );
-              })}
+              return (
+                <Character
+                  key={`${wordIndex}-${charIndex}`}
+                  char={char}
+                  typedCharacter={typedCharacter}
+                  isCurrentChar={isCurrentChar}
+                  activeCharRef={activeCharRef}
+                />
+              );
+            })}
 
-              {/* REAL SPACE */}
-              {wordIndex !== words.length - 1 && <span> </span>}
-            </div>
-          );
-        })}
-        </div>
-  )
-}
+            {/* REAL SPACE */}
+            {wordIndex !== words.length - 1 && <span> </span>}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default TypingArea
+export default TypingArea;
