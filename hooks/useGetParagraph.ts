5f81cd1e3a49eae1f19f98197989980f1684ccac
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-06-22 00:05:34 +0530

    feat(hooks): add a hook to fetch paragraph

---
+++ hooks/useGetParagraph.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export default function useGetParagraph() {
+    const [para,setPara] = useState("");
+    useEffect(() => {
+      async function loadParagraph() {
+        const res = await fetch("/api/paragraph");
+        const data = await res.json();
+    
+        setPara(data.text);
+      }
+    
+      loadParagraph();
+      console.log("Paragraph fetched");
+    }, []);
+    return para;
+}
