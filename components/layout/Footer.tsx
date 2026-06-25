e57c9e0bdfb13d81fbb19e1abccf2be3f448a371
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-06-26 01:51:20 +0530

    feat(components): add footer component

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='ml-6'><h3>A project by <Link href="https://yashmishra.vercel.app" className='text-(--accent)'>Yash Mishra</Link></h3></footer>
+  )
+}
+
+export default Footer
