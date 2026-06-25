0ca00d9087bbd2b154a5cc0f7997cfa907c58255
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-06-26 01:59:53 +0530

    refactor(components): restyle reset component with icon

--- components/main/Reset.tsx
+++ components/main/Reset.tsx
@@ -1,7 +1,8 @@
+import { GrPowerReset } from "react-icons/gr"
 
 const Reset = ({reset}:{reset:(e:React.MouseEvent<HTMLButtonElement>)=>void}) => {
   return (
-    <button onClick={reset} className="hover:cursor-pointer border p-2 rounded-xl hover:scale-110 transition-all duration-100 ease-in-out">reset</button>
+    <button onClick={reset} className="hover:cursor-pointer border p-2 rounded-xl hover:scale-110 transition-all duration-100 ease-in-out flex items-center gap-1 px-4 hover:text-(--accent)"><GrPowerReset /></button>
   )
 }
 
